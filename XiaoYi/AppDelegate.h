fde7465c380f3422b8cd6280f37bb48eb0d4b419
wurs <522970682@qq.com>
2016-06-25 21:06:58 +0800

    创建项目

---
+++ XiaoYi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiaoYi
+//
+//  Created by rason on 16/6/25.
+//  Copyright © 2016年 RasonWu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
